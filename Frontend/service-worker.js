6ff5ae325647f6abeba6c77e0c8224d67c828798
Emmanuel Ogbaa <ogbaae@.com>
2024-12-30 13:17:23 +0100

    mRate

--- Frontend/service-worker.js
+++ Frontend/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mai-cache-v7.09'; // Cache name
-const DYNAMIC_CACHE_NAME = 'dynamic-cache-v7.09'; // Dynamic cache name
+const CACHE_NAME = 'mai-cache-v7.1.0'; // Cache name
+const DYNAMIC_CACHE_NAME = 'dynamic-cache-v7.1.0'; // Dynamic cache name
 
 // Files to cache manually
 const STATIC_FILES = [
