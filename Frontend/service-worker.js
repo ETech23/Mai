61b5697f0e71991b9c95496c498d55fa46b4bb68
ETech23 <ogbaae@gmail.cm>
2025-02-27 21:02:56 +0100

    api-cache

--- Frontend/service-worker.js
+++ Frontend/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mai-cache-v8.1.6';
-const DYNAMIC_CACHE_NAME = 'dynamic-cache-v8.1.6';
+const CACHE_NAME = 'mai-cache-v8.1.7';
+const DYNAMIC_CACHE_NAME = 'dynamic-cache-v8.1.7';
 const STATIC_FILES = ['/offline.html'];
 
 self.addEventListener('install', (event) => {
