424115139b9151a5742de813f23b03dd3f7e4bc6
ETech23 <ogbaae@gmail.cm>
2025-03-26 14:38:26 +0100

    env

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const { OAuth2Client } = require("google-auth-library");
-const nodemailer = require("nodemailer");
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;             const client = new OAuth2Client(CLIENT_ID);
 
